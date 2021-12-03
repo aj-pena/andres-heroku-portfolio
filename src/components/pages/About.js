58b19912bfac5e77ffb3eff77afe3b978683d84e
Andres Pena <penchjpc@gmail.com>
2021-12-03 18:45:36 -0500

    updated about me brand

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ function About() {
                 </div>
                 {/* <!-- content --> */}
                 <div className="col-12 col-sm-6 col-md-8 col-lg-8">
-                  <p id="about-me"> I have 8 years of experience in transportation planning and engineering. Registered as EIT with Professional Engineers Ontario, I have taken formal training in ArcMap, Vissim, AutoCAD, Revit and Photoshop. Passionate about urban and regional transportation, I am currently learning to code web applications that will help cities and companies move safely, efficiently and fairly. </p>    
+                  <p id="about-me"> Web developer leveraging transportation planning background to build web applications for cities and companies. Recently earned a Certificate in Full-stack Development from the University of Toronto, with newly developed skills in JavaScript, CSS, React.js, SQL, MongoDB, Express and NodeJS. Known as an integral analyst passionate about front-end design. I applied aspects of responsive design in a recent project. I worked on a team of five to develop a multi-page MERN app that helps students from underserved communities find funds to advance their education. I am excited to leverage my skills as part of a double-impact, quality-driven team to improve cities and companies logistics through immersive web applications. </p>    
               </div>
             </div>
         </div>
