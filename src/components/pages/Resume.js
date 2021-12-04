bcdeefc61bd6920ce34c586398645b1fb534682b
Andres Pena <penchjpc@gmail.com>
2021-12-03 19:36:44 -0500

    Updated skills section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,14 +5,30 @@ function Resume() {
     return (
         <div className= 'resume'>
             <h4>Proficiencies</h4>
-            <ul className='resumeUl'>
-                <li className= 'resumeList'>HTML</li>
-                <li className= 'resumeList'>CSS</li>
-                <li className= 'resumeList'>JavaScript</li>
-                <li className= 'resumeList'>ExpressJs</li>
-                <li className= 'resumeList'>NodeJs</li>
-            </ul>
-            <a href={pdf}>Download a copy of my Resume</a>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-lg-4">
+                        <ul className='resumeUl'>
+                            <li className= 'resumeList'><span >&#9899;</span>HTML</li>
+                            <li className= 'resumeList'><span >&#9899;</span>CSS</li>
+                            <li className= 'resumeList'><span >&#9899;</span>JavaScript</li>
+                            <li className= 'resumeList'><span >&#9899;</span>ExpressJs</li>
+                            <li className= 'resumeList'><span >&#9899;</span>NodeJs</li>
+                        </ul>
+                    </div>
+                    <div className="col-12 col-lg-4">
+                        <ul className='resumeUl'>
+                            <li className= 'resumeList'><span >&#9899;</span>SQL</li>
+                            <li className= 'resumeList'><span >&#9899;</span>NoSQL</li>
+                            <li className= 'resumeList'><span >&#9899;</span>MongoDb</li>
+                            <li className= 'resumeList'><span >&#9899;</span>React</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <a className="copy-resume" href={pdf}>Download a copy of my Resume</a>
+            <br/>
         </div>
     )
 }
