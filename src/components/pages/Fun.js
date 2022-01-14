e11939059ecdc6b0b1b1fd1e11f0abca9a3a3cf6
Andres Pena <penchjpc@gmail.com>
2022-01-14 14:47:43 -0500

    addedd titles and images to Fun Cards

--- src/components/pages/Fun.js
+++ src/components/pages/Fun.js
@@ -1,63 +1,70 @@
 import React from 'react'
 import Hobby from '../Hobby'
 // Import images of projects
-
+import Zero from '../../Assets/images/Zero-gravity.jpg'
+import First from '../../Assets/images/FirstFewDays.png'
+import Cat from '../../Assets/images/Cat.JPG'
+import TreeLamp from '../../Assets/images/TreeLamp.jpg'
+import MyHand from '../../Assets/images/MyHand.jpg'
+import Bench from '../../Assets/images/Bench.jpg'
+import Building from '../../Assets/images/Building.jpg'
+import Hand from '../../Assets/images/Hand.jpg'
 
 const diyProjects = [
   {
-      title: 'Movie Pal',
+      title: 'Zero-Gravity Park Bench',
       page: 'https://altugcakmakci.github.io/movie_pal/',
       repo: 'https://github.com/altugcakmakci/movie_pal.git',
-      image: MoviePal,
+      image: Zero,
       description: 'This application allows the cinephile to search for a title from the IDB movie database. It also allows the user to search for an actor or director. Each search returns a list of options to choose from.',
   },
   {
-      title: 'In or Out',
+      title: 'Moving to Ottawa',
       page: 'https://inout-app.herokuapp.com/',
       repo: 'https://github.com/cathytanya/inout.git',
-      image: InOrOut,
+      image: First,
       description: 'This web application will allow users to first choose if they want to stay inside or if they want to go outside. Once chosen, the application will display a set of activity cards and present the user with preference buttons to filter activities.',
   },
   {
-      title: 'Day Planer',
+      title: 'Cat in Ottawa',
       page: 'https://aj-pena.github.io/Day-Planner/',
       repo: 'https://github.com/aj-pena/Day-Planner.git',
-      image: DayPlanner,
+      image: Cat,
       description: 'This application allows the user to schedule events for a 5 day time frame. It contains slots for every work-day hour that can be used to store event names. The times-slots are color-coded to represent past, present and future time-slots.',
   },
   {
-      title: 'Code Quiz',
+      title: 'Laser-Cutted Tree Lamp',
       page: 'https://aj-pena.github.io/Code-Quiz/',
       repo: 'https://github.com/aj-pena/Code-Quiz.git',
-      image: CodeQuiz,
+      image: TreeLamp,
       description: 'This program will generate an interactive quiz with multiple choices that will keep track of high scores. This program will allow the user to keep his/her coding knowledge greased and compare it to others via scores.',
   },
   {
-      title: 'Progressive Budget',
+      title: 'Sculpting My Hand',
       page: 'https://budget-tracker-ajp.herokuapp.com/',
       repo: 'https://github.com/aj-pena/Budget-Tracker-offline.git',
-      image: BudgetTracker,
+      image: MyHand,
       description: 'This application will allow an avid traveler record their transactions even if they are offline.',
   },
   {
-      title: 'Fitness Tracker',
+      title: 'Bench',
       page: 'https://fitness-tracker-apj.herokuapp.com/?id=618d6d09e1bbb1001665fd93',
       repo: 'https://github.com/aj-pena/Fitness-Tracker.git',
-      image: FitnessTracker,
+      image: Bench,
       description: 'This application allows the user to create workouts and add exercises to each workout. By keeping track of the number of workouts and time spent, the user can reach his/her goals faster.',
   },
   {
-    title: 'Google Books Search Engine',
+    title: 'Conceptualizing Spaces',
     page: 'https://googlebooks-ajp.herokuapp.com/',
     repo: 'https://github.com/aj-pena/Book-search-engine.git',
-    image: GoogleBooks,
+    image: Building,
     description: 'This application is built on the cloud with a MERN Stack framework and uses the Google Book API. It allows the avid reader to search for books of interest and to add them to a personal collection.',
   },
   {
-    title: 'Scholars4Dollars',
+    title: 'Life Drawing',
     page: 'https://aqueous-ocean-45024.herokuapp.com/',
     repo: 'https://github.com/giftilicious/scholarship-app.git',
-    image: Scholars4Dollars,
+    image: Hand,
     description: 'MERN web application that helps underserved students find funding for college or university',
   }
 
